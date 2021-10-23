0f01c3031e21414152f1668010ad2cc2805d73db
David <davidkao41@ucla.edu>
2021-10-22 19:41:55 -0700

    lab 3 mqtt task

--- publisher.py
+++ publisher.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import numpy as np
 
 # 0. define callbacks - functions that run when events happen.
 # The callback for when the client receives a CONNACK response from the server.
@@ -7,8 +8,8 @@ def on_connect(client, userdata, flags, rc):
     
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("ece180d/test", qos=1)
-
+    # client.subscribe("ece180d/test")
+    
 # The callback of the client when it disconnects.
 def on_disconnect(client, userdata, rc):
     if rc != 0:
@@ -17,13 +18,12 @@ def on_disconnect(client, userdata, rc):
         print("Expected Disconnect")
 
 # The default message callback.
-# (you can create separate callbacks per subscribed topic)
+# (won’t be used if only publishing, but can still exist)
 def on_message(client, userdata, message):
-    print("Received message: '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
+    print('Received message: "' + str(message.payload) + '" on topic "' + message.topic + '" with QoS ' + str(message.qos))
 
 # 1. create a client instance.
 client = mqtt.Client()
-
 # add additional client options (security, certifications, etc.)
 # many default options should be good to start off.
 # add callbacks to client.
@@ -32,16 +32,19 @@ client.on_disconnect = on_disconnect
 client.on_message = on_message
 
 # 2. connect to a broker using one of the connect*() functions.
-client.connect_async("mqtt.eclipseprojects.io")
+client.connect_async('mqtt.eclipseprojects.io')
 
 # 3. call one of the loop*() functions to maintain network traffic flow with the broker.
 client.loop_start()
-print("subscribe start")
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    pass
+
+# 4. use subscribe() to subscribe to a topic and receive messages.
+# 5. use publish() to publish messages to the broker.
+# payload must be a string, bytearray, int, float or None.
+for i in range(10):
+    j = float(np.random.random(1))
+    print(j)
+    client.publish('ece180d/test', j , qos=1)
     
+# 6. use disconnect() to disconnect from the broker.
 client.loop_stop()
 client.disconnect()
--- serverTest.py
+++ serverTest.py
@@ -1,16 +1,22 @@
 import socket
-serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serv.bind(('0.0.0.0', 8080))
-serv.listen(5)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+host_ip = socket.gethostbyname(host)
+port = 8080
+s.bind((host_ip, port))
+
+s.listen(5)
 while True:
-    conn, addr = serv.accept()
+    c, addr = s.accept()
     from_client = ''
+    print('Got connection from ', addr)
     while True:
-        data = conn.recv(4096)
+        data = c.recv(4096)
         if not data: break
-        from_client += data
-        print(from_client)
-        conn.send("I am SERVER\n")
-    conn.close()
-    print('client disconnected')
+        from_client = data
+        print(from_client.decode('ascii'))
+        c.send("I am SERVER".encode())
+    c.close()
+    print('Client disconnected')
+    break
